6e33a71114f326e5a93b540901b29ac72f7c9f6a
Venugopal Reddy Koduru <koduru2463@sdsu.edu>
2024-01-21 02:33:42 +0530

    artist/album data

--- Raw Data/Artist.py
+++ Raw Data/Artist.py
@@ -1,4 +1,5 @@
 import requests
+
 def get_spotify_token(client_id, client_secret):
     """
     Get Spotify access token using client credentials flow.
@@ -12,40 +13,39 @@ def get_spotify_token(client_id, client_secret):
         "client_id": client_id,
         "client_secret": client_secret,
     }
- 
+
     response = requests.post(token_url, headers=headers, data=data)
     token_data = response.json()
     access_token = token_data.get("access_token")
     return access_token
- 
-def get_album_info(album_id, access_token):
+
+def get_artist_info(artist_id, access_token):
     """
-    Get information about a Spotify album using the album ID.
+    Get information about a Spotify album using the artist ID.
     """
-    album_url = f"https://api.spotify.com/v1/albums/{album_id}"
+    artist_url = f"https://api.spotify.com/v1/artists/{artist_id}"
     headers = {
         "Authorization": f"Bearer {access_token}",
     }
- 
-    response = requests.get(album_url, headers=headers)
-    album_data = response.json()
-    return album_data
- 
+
+    response = requests.get(artist_url, headers=headers)
+    artist_data = response.json()
+    return artist_data
+
 def main():
-    
     # Replace these with your actual values
     client_id = "31c2e953a1cc49eaa153d1fd504728a3"
     client_secret = "eb2ee00f392f49bf850c16deb6e824ce"
     access_token = get_spotify_token(client_id, client_secret)
- 
+
     # Replace these with your actual album IDs
-    album_ids = ["382ObEPsp2rxGrnsizN5TX", "1A2GTWGtFfWp7KSQTwWOyo", "2noRn2Aes5aoNVsU6iWThc"]
- 
-    for album_id in album_ids:
-        album_info = get_album_info(album_id, access_token)
-        print(f"\nAlbum ID: {album_id}")
-        print("Album Information:")
-        print(album_info)
- 
+    artist_ids = ["2CIMQHirSU0MQqyYHq0eOx", "57dN52uHvrHOxijzpIgu3E", "1vCWHaC5f2uS3yhpwWbIA6"]
+
+    for artist_id in artist_ids:
+        artist_info = get_artist_info(artist_id, access_token)
+        print(f"\n Artist ID: {artist_id}")
+        print("Artist Information:")
+        print(artist_info)
+
 if __name__ == "__main__":
     main()
